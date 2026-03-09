a04777c7f1ce7609717823de18482b397eef458b
profLedson <ledson.vanini@escola.pr.gov.br>
2026-03-09 13:30:37 +0000

    Introdução aos Arrays em JS

---
+++ arrays/array-time.js
@@ -0,0 +1,10 @@
+// Array: são agrupamentos de valores em forma de lista
+// São indexados a partir de ZERO até o infinito
+// Podem ter valores homegêneos/heterogêneos
+// Possuem métodos (funções) próprias
+
+const time = ['Petter', 'Mariah']
+  // indeices    0         1
+
+console.log(time[1])
+console.log(time[32]) //undefined
---
+++ condicionais/if-else.js
@@ -0,0 +1,16 @@
+// if, else if, else, ternário
+// >, <, >=, <=, !=
+// ||, &&
+
+let idade = 116
+
+if(idade > 110){
+    console.log("Situação ímpar")
+    console.log("Traga um acompanhante")
+} else if (idade >= 17 && idade <= 110 ) {
+    console.log("Entrada Liberada")
+    console.log("Festa sugerida: Balada!!!")
+} else {
+    console.log("Proibido, vaza menor!")
+    console.log("Festa recomenda: Monica Toy")
+}
