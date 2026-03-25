8d98681c85c2d93167e1a8ba4021e01f8f9707d8
profLedson <ledson.vanini@escola.pr.gov.br>
2026-03-25 11:37:47 +0000

    Finalização de Objetos JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,14 @@
+// Objetos são estruturas chave-valor 
+// Também chamadas "key-value"
+// Objetos podem conter: arrays, string,
+// number, objects, functions...
+// possuem métodos próprios
+
+const jogador = {
+    nome: "Lielson",
+    gols: 292,
+    times: ['Palmeira', 'Coxa', 'Flamengo','Corinthians']
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.times[1])
---
+++ objetos/obj-02.js
@@ -0,0 +1,6 @@
+// A partir do Objeto de exemplo crie:
+// Um objeto com o tema à sua escola
+// deve possuir no mínimo 6 propriedades
+// imprima de um jeito amigável:
+// Ex.: console.log("Cor do veículo: " + carro.cor)
+
